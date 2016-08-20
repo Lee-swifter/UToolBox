70bf7aee80f4340fc860513006d13cf3b9c8bb2d
Swifter <chengkong2006@126.com>
2016-08-20 20:39:41 +0800

    simplify code

--- app/src/main/java/lic/swifter/box/util/ToastUtil.java
+++ app/src/main/java/lic/swifter/box/util/ToastUtil.java
@@ -8,19 +8,8 @@ import android.widget.Toast;
  * Created by lic on 16-7-4.
  */
 public class ToastUtil {
-    private static Toast toast;
 
-    /**
-     * 短时间显示Toast
-     */
-    public static void showShort(Context context, CharSequence message) {
-        if (null == toast) {
-            toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-        } else {
-            toast.setText(message);
-        }
-        toast.show();
-    }
+    private static Toast toast;
 
     /**
      * 短时间显示Toast
@@ -28,71 +17,10 @@ public class ToastUtil {
     public static void showShort(Context context, int message) {
         if (null == toast) {
             toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-            // toast.setGravity(Gravity.CENTER, 0, 0);
         } else {
             toast.setText(message);
         }
         toast.show();
     }
 
-    /**
-     * 长时间显示Toast
-     */
-    public static void showLong(Context context, CharSequence message) {
-        if (null == toast) {
-            toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-            // toast.setGravity(Gravity.CENTER, 0, 0);
-        } else {
-            toast.setText(message);
-        }
-        toast.show();
-    }
-
-    /**
-     * 长时间显示Toast
-     */
-    public static void showLong(Context context, int message) {
-        if (null == toast) {
-            toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
-            // toast.setGravity(Gravity.CENTER, 0, 0);
-        } else {
-            toast.setText(message);
-        }
-        toast.show();
-    }
-
-    /**
-     * 自定义显示Toast时间
-     */
-    public static void show(Context context, CharSequence message, int duration) {
-        if (null == toast) {
-            toast = Toast.makeText(context, message, duration);
-            // toast.setGravity(Gravity.CENTER, 0, 0);
-        } else {
-            toast.setText(message);
-        }
-        toast.show();
-    }
-
-    /**
-     * 自定义显示Toast时间
-     */
-    public static void show(Context context, int message, int duration) {
-        if (null == toast) {
-            toast = Toast.makeText(context, message, duration);
-            // toast.setGravity(Gravity.CENTER, 0, 0);
-        } else {
-            toast.setText(message);
-        }
-        toast.show();
-    }
-
-    /**
-     * Hide the toast, if any.
-     */
-    public static void hideToast() {
-        if (null != toast) {
-            toast.cancel();
-        }
-    }
 }
