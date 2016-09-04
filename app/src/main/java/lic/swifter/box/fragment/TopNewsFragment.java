fc07cd39e2f2fe0a1b09b4ab84ed7340881479ef
Swifter <chengkong2006@126.com>
2016-09-05 00:01:15 +0800

    add layout

--- app/src/main/java/lic/swifter/box/fragment/TopNewsFragment.java
+++ app/src/main/java/lic/swifter/box/fragment/TopNewsFragment.java
@@ -2,10 +2,14 @@ package lic.swifter.box.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import lic.swifter.box.R;
 import lic.swifter.box.api.model.Result;
 import lic.swifter.box.api.model.TopNewsWrapper;
 import lic.swifter.box.mvp.presenter.NetQueryType;
@@ -32,12 +36,24 @@ import lic.swifter.box.mvp.view.IView;
 
 public class TopNewsFragment extends BaseFragment implements IView<String, TopNewsWrapper> {
 
+    @Bind(R.id.fragment_top_news_view_pager)
+    ViewPager viewPager;
     private NewsPresenter presenter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_top_news, container, false);
+        ButterKnife.bind(this, rootView);
+        presenter = new NewsPresenter(this);
+
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        ButterKnife.unbind(this);
+        super.onDestroyView();
     }
 
     @Override
