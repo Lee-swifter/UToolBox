7d6182e7210ad70e2219644289a479d028548529
Swifter <chengkong2006@126.com>
2017-03-11 09:18:16 +0800

    modify mobile info display

--- app/src/main/java/lic/swifter/box/activity/MainActivity.java
+++ app/src/main/java/lic/swifter/box/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -56,12 +57,20 @@ public class MainActivity extends AppCompatActivity implements ToolsAdapter.OnIt
 
     private long exitTimeStamp;
 
+    public static int screenWidth;
+    public static int screenHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initView();
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        screenWidth = dm.widthPixels;
+        screenHeight = dm.heightPixels;
     }
 
     @Override
