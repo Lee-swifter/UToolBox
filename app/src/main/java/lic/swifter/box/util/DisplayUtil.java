ca36b6fc74c260736cff7ba448b196d9044c4e7a
李超 <lichao@okay.cn>
2017-09-08 17:20:50 +0800

    add method to DisplayUtil

--- app/src/main/java/lic/swifter/box/util/DisplayUtil.java
+++ app/src/main/java/lic/swifter/box/util/DisplayUtil.java
@@ -7,8 +7,13 @@ import android.content.Context;
  */
 public class DisplayUtil {
 
-    public static int dip2px(Context context, float dip) {
+    public static int dip2px(Context context, float dipValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (dip * scale + 0.5);
+        return (int) (dipValue * scale + 0.5);
+    }
+
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
     }
 }
